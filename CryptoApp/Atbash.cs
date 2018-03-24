63b8792d742b7b5fae7d2da2eac9d58830c7d7cd
DESKTOP-QAAAD41\DeepBrain <andrii.karachevtsev@gmail.com>
2018-03-24 21:57:23 +0200

    basic logic added

---
+++ CryptoApp/Atbash.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptoApp
+{
+    public static class Atbash
+    {
+        private const string DefaultAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        public static string Decode(string input, string alphabet = DefaultAlphabet)
+        {
+            var plain = alphabet.ToUpperInvariant();
+            return Transform(input, plain);
+        }
+
+        public static string Encode(string input, string alphabet = DefaultAlphabet)
+        {
+            var cipher = new string(alphabet.ToUpperInvariant().Reverse().ToArray());
+            return Transform(input, cipher);
+        }
+
+        private static string Transform(string input, string code)
+        {
+            var letters = new List<char>(input.Length);
+            foreach (var c in input.ToUpperInvariant())
+            {
+                if (char.IsLetter(c))
+                {
+                    var index = code.Length - code.IndexOf(c) - 1;
+                    letters.Add(code[index]);
+                }
+            }
+            return new string(letters.ToArray());
+        }
+    }
+}
---
+++ CryptoApp/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows;
+
+namespace CryptoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string radioButtonState = "";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string encodeStr = textBox1.Text;
+            switch (radioButtonState)
+            {
+                case "Caesar":
+                    
+                    int key = Convert.ToInt32(keyTextBox.Text);
+                    string decodeStr = Caesar.Decipher(encodeStr, key);
+                    textBox2.Text = decodeStr;
+                    break;
+
+                case "Atbash":
+                    string decode = Atbash.Decode(encodeStr);
+                    textBox2.Text = decode;
+                    break;
+
+                case "Viziner":
+                    string keyViziner = keyTextBox.Text;
+                    string decodeViziner = Viziner.Decipher(encodeStr, keyViziner);
+                    textBox2.Text = decodeViziner;
+                    break;
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            this.radioButtonState = "Atbash";
+            this.keyLabel.Visibility = Visibility.Hidden;
+            this.keyTextBox.Visibility = Visibility.Hidden;
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            this.radioButtonState = "Caesar";
+            this.keyLabel.Visibility = Visibility.Visible;
+            this.keyTextBox.Visibility = Visibility.Visible;
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            this.radioButtonState = "Viziner";
+            this.keyLabel.Visibility = Visibility.Visible;
+            this.keyTextBox.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string encodeStr = textBox1.Text;
+            switch (radioButtonState)
+            {
+                case "Atbash":
+                    MessageBox.Show("Atbash");
+                    textBox2.Text = Atbash.Encode(encodeStr);
+                    break;
+                case "Caesar":
+                    MessageBox.Show("Caesar");
+                    int key = Convert.ToInt32(keyTextBox.Text);
+                    textBox2.Text = Caesar.Encipher(encodeStr, key);
+                    break;
+                case "Viziner":
+                    MessageBox.Show("Viziner");
+                    string keyViziner = keyTextBox.Text;
+                    textBox2.Text = Viziner.Encipher(encodeStr, keyViziner);
+                    break;
+            }
+        }
+    }
+}
